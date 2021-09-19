8066a484820c22a45eb5826f68607825441af0f1
Leonardo Burtet <leonardoburtet@outlook.com>
2021-09-19 12:57:45 -0300

    refactor js navMenu

--- script/navMenu.js
+++ script/navMenu.js
@@ -1,16 +1,24 @@
 export default function navMenu() {
   const menuSpan = document.querySelectorAll(".box__menu ul span");
-  const menuUl = document.querySelectorAll(".nav-list");
+  const menuUl = document.querySelectorAll(".box__menu ul li ul");
+  const menuIcon = document.querySelectorAll(".box__menu ul li i");
 
-  console.log(menuUl);
-  if (menuSpan.length) {
-    menuSpan.forEach((element) => {
-      element.addEventListener("click", handleClickActive);
-    });
-    function handleClickActive() {
-      this.classList.toggle("ativo");
-      this.nextElementSibling.classList.toggle("show");
-      this.lastElementChild.classList.toggle("icon-rotate");
+  menuSpan.forEach((element) => {
+    element.addEventListener("click", handleClickDesktop);
+  });
+  function handleClickDesktop() {
+    console.log(this.classList);
+    if (this.classList[0] === "ativo") {
+      this.classList.remove("ativo");
+      this.nextElementSibling.classList.remove("show");
+      this.lastElementChild.classList.remove("icon-rotate");
+    } else {
+      menuSpan.forEach((e) => e.classList.remove("ativo"));
+      menuUl.forEach((e) => e.classList.remove("show"));
+      menuIcon.forEach((e) => e.classList.remove("icon-rotate"));
+      this.classList.add("ativo");
+      this.nextElementSibling.classList.add("show");
+      this.lastElementChild.classList.add("icon-rotate");
     }
   }
 }
